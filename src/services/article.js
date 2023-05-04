ebe595d385d7ece994887db3c454a01c043fb1f1
Celine Liu <zijingliu2021@outlook.com>
2023-05-03 18:16:47 -0700

    deleted unused code

--- src/services/article.js
+++ src/services/article.js
@@ -3,27 +3,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const RapidAPIKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-// const options = {
-//   method: "GET",
-//   url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
-//   params: {
-//     url: "https://time.com/6266679/musk-ai-open-letter/",
-//     length: "3",
-//   },
-//   headers: {
-//     "content-type": "application/octet-stream",
-//     "X-RapidAPI-Key": "82fdbb238dmshdcf79e76b2829eep1d9440jsnc5e3b23048c0",
-//     "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
-//   },
-// };
-
-// try {
-//   const response = await axios.request(options);
-//   console.log(response.data);
-// } catch (error) {
-//   console.error(error);
-// }
-
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
